113b46127e7fce4ed9f40d4ad14aa791f28ad867
Santosh Pandey <santosh.pandey@idc.bedbath.com>
2022-01-05 21:40:17 +0530

    fix: fix translation message

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Navigation from './Navigation';
 
-export default function Header({ children, title }) {
+export default function Header() {
    return (
     <header className="border-b sticky top-0 z-20 bg-th-background-secondary">  
         <Navigation/>
--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { useTranslations } from 'next-intl'
+
 import { useRouter } from 'next/router'
 import Footer from './Footer'
 import Header from './Header'
 
 export default function Layout({ children }) {
-  const t = useTranslations('Index');
-  const { locale } = useRouter();
+  
+  
   return (
     <div className="flex flex-col justify-between min-h-screen">
-      <Header title={t('title')} locale={locale} />
+      <Header />
       <main className='bg-th-background'>
         {children}
       </main>
